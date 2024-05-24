98232700a6bbe5dafdfeeaf1c46a1503aee79978
naqet <naqet8@gmail.com>
2024-05-24 08:58:39 +0200

    feat: added logger service

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/naqet/htmx-events-app
+module htmx-events-app
 
 go 1.22.3
--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"htmx-events-app/services/logger"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	log := logger.NewLogger()
+
+	mux.Handle("/{$}", log.Middleware(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	}))
+
+	mux.Handle("/health", log.Middleware(func(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte(http.StatusText(http.StatusOK)))
+	}))
+
+	mux.Handle("/", log.Middleware(http.NotFound))
+
+	http.ListenAndServe("localhost:3000", mux)
+}
