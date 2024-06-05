50c7384e5a0950a78915001d0b39cb7a2ab13f69
naqet <naqet8@gmail.com>
2024-06-05 20:41:37 +0200

    feat: updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,38 @@
 const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.templ"],
-  theme: {
-    extend: {
-        colors: {
-            "secondary": colors.stone[50]
-        }
+    content: ["./views/**/*.templ"],
+    theme: {
+        extend: {
+            colors: {
+                "secondary": colors.stone[50]
+            },
+            keyframes: {
+                appear: {
+                    "0%": {
+                        opacity: 0,
+                        transform: `translateX(10px) scale(0.95)`,
+                    },
+
+                    "100%": {
+                        opacity: 100,
+                        transform: `translateX(0) scale(1)`,
+                    }
+                }
+            },
+            animation: {
+                appear: 'appear 300ms'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
+    safelist: [
+        "animation-appear",
+        "toast",
+        "toast-success",
+        "toast-info",
+        "toast-danger",
+        "toast-message",
+    ]
 }
 
